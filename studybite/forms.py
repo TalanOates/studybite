7990ddb4c494b1f11cf9ab032810852d5cdc3ea7
TalanOates <43966364+TalanOates@users.noreply.github.com>
2019-04-03 11:52:19 +0100

    Add files via upload

---
+++ studybite/forms.py
@@ -0,0 +1,59 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from studybite.models import User
+import string
+
+class RegForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username already taken')
+
+class LogForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class PictureForm(FlaskForm):
+    picture = FileField('Files allowed : jpg,png', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+class PollForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    submit = SubmitField('Post')
+    topic1 = StringField('First Vote Topic', validators=[DataRequired()])
+    topic2 = StringField('second Vote Topic', validators=[DataRequired()])
+    topic3 = StringField('Third Vote Topic')
+    topic4 = StringField('Forth Vote Topic')
+    category = SelectField('category', choices=[("school_work", "School Work"),
+     ("home_work", "Home work"), ("out_of_school", "Out of school"), ("Misc", "Misc")])
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+    category = SelectField('category', choices=[("school_work", "School Work"),
+     ("home_work", "Home work"), ("out_of_school", "Out of school"), ("Misc", "Misc")])
+
+class RequestForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Search')
+
+class GroupForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Create')
+
+class ReplyForm(FlaskForm):
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
